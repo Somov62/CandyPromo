998437d2be87c159841fc5548865580a2d2efa3b
somov <somov@pulsarm.ru>
2024-12-05 22:52:59 +0300

    #10 Добавил генератор промокодов

--- CandyPromo.Data.MigrationService/GlobalUsings.cs
+++ CandyPromo.Data.MigrationService/GlobalUsings.cs
@@ -1,10 +1,4 @@
 ﻿global using CandyPromo.Data.Contexts;
 global using CandyPromo.Data.MigrationService;
-
-global using Microsoft.EntityFrameworkCore.Storage;
-
-global using OpenTelemetry.Trace;
-
-global using System.Diagnostics;
 global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Infrastructure;
+global using System.Diagnostics;
