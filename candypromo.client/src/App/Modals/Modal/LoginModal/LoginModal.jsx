29a5be8de51c4f0f9c418453e241e637c0858438
Дмитрий <dimanrusdevnet@outlook.com>
2025-02-02 11:26:29 +0300

    Update

--- candypromo.client/src/App/Modals/Modal/LoginModal/LoginModal.jsx
+++ candypromo.client/src/App/Modals/Modal/LoginModal/LoginModal.jsx
@@ -1,97 +1,117 @@
-import {useState, useEffect} from 'react';
-import './LoginModal.css';
-import {Dialog} from "primereact/dialog";
-import {InputText} from "primereact/inputtext";
-import {Password} from 'primereact/password';
-import {Button} from 'primereact/button';
+import { useState, useRef } from "react";
+import "./LoginModal.css";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
+import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
+import { TabMenu } from "primereact/tabmenu";
 import axios from "axios";
 
 /*
-    Компонент - модальное окно входа
-    Входные параметры:
-    - openState - хук useState(true | false) на отображение модалки.
-*/
+ Компонент - модальное окно входа
+ Входные параметры:
+ - openState - хук useState(true | false) на отображение модалки.
+ */
 
-function LoginModal({openState, navigateToRegisterPage}) {
+function LoginModal({ openState, navigateToRegisterPage }) {
+    const [selectedTabId, setSelectedTabId] = useState(0);
     const [isLoading, setIsLoading] = useState(false);
+    const [userLogin, setUserLogin] = useState("");
+    const [userPassword, setUserPassword] = useState("");
 
-    const [userLogin, setUserLogin] = useState('');
-    const [userPassword, setUserPassword] = useState('');
-
-    const [userLoginError, setUserLoginError] = useState('');
-    const [userPasswordError, setUserPasswordError] = useState('');
-
-    useEffect(() => {
-        if (isLoading) {
-            setUserLoginError('');
-            setUserPasswordError('');
-        }
-    }, [isLoading]);
+    const toast = useRef(null);
 
     return (
-        <Dialog
-            className="login-modal"
-            header="Вход"
-            visible={openState.value}
-            style={{width: "40%"}}
-            onHide={() => {
-                if (!openState.value) return;
-                openState.set(false);
-            }}>
-            <div className="flex flex-column gap-2">
-                <label htmlFor="username">Логин</label>
-                <InputText
-                    id="username"
-                    aria-describedby="username-help"
-                    value={userLogin}
-                    onChange={(e) => {
-                        setUserLogin(e.target.value);
-                        setUserLoginError('')
-                    }}
-                    disabled={isLoading}/>
-                <label className="error-help">{userLoginError}</label>
-            </div>
-            <div className="flex flex-column gap-2">
-                <label htmlFor="password">Пароль</label>
-                <Password
-                    id="password"
-                    aria-describedby="password-help"
-                    value={userPassword}
-                    onChange={(e) => {
-                        setUserPassword(e.target.value);
-                        setUserPasswordError('')
-                    }}
-                    feedback={false}
-                    disabled={isLoading}/>
-                <label className="error-help">{userPasswordError}</label>
-            </div>
-            <Button
-                className="login-button"
-                label="Войти"
-                loading={isLoading}
-                onClick={() => login()}/>
-            <Button
-                className="link-button"
-                label="Создать аккаунт"
-                link
-                onClick={navigateToRegisterPage}/>
-        </Dialog>);
+        <div>
+            <Toast
+                ref={toast}
+                position="top-center" />
+            <Dialog
+                className="login-modal"
+                header="Вход"
+                visible={openState.value}
+                style={{ width: "50vw" }}
+                onHide={() => {
+                    if (!openState.value) return;
+                    openState.set(false);
+                }}>
+                <TabMenu
+                    className="TabMenu mb-3"
+                    model={[{ label: "Телефон" }, { label: "Электронная почта" }]}
+                    disabled={loading}
+                    activeIndex={selectedTabId}
+                    onTabChange={(e) => setSelectedTabId(e.index)} />
+                <div className="flex flex-column gap-2">
+                    <label htmlFor="username">Логин</label>
+                    <InputText
+                        id="username"
+                        aria-describedby="email-help"
+                        value={userLogin}
+                        onChange={(e) => {
+                            setUserLogin(e.target.value);
+                            document.getElementById("email-help").innerText = "";
+                        }}
+                        disabled={isLoading} />
+                    <div className="error-help">
+                        <label id="email-help" />
+                    </div>
+                </div>
+                <div className="flex flex-column gap-2">
+                    <label htmlFor="password">Пароль</label>
+                    <Password
+                        id="password"
+                        aria-describedby="password-help"
+                        value={userPassword}
+                        onChange={(e) => {
+                            setUserPassword(e.target.value);
+                            document.getElementById("password-help").innerText = "";
+                        }}
+                        feedback={false}
+                        disabled={isLoading} />
+                    <div className="error-help">
+                        <label id="password-help" />
+                    </div>
+                </div>
+                <Button
+                    className="login-button"
+                    label="Войти"
+                    loading={isLoading}
+                    onClick={() => login()} />
+                <Button
+                    className="link-button"
+                    label="Создать аккаунт"
+                    link
+                    onClick={navigateToRegisterPage}
+                    disabled={isLoading} />
+            </Dialog>
+        </div>);
 
     async function login() {
         setIsLoading(true);
+        document.getElementById("email-help").innerText = document.getElementById("password-help").innerText = "";
         await axios
-            .post(`/api/auth/login`, {email: userLogin, password: userPassword})
+            .post(`/api/auth/login`, { email: userLogin, password: userPassword })
             .then((response) => {
                 const token = response.data;
                 localStorage.setItem("token", token);
-                console.log(response.data);
+                console.log(token);
+                toast.current.show({
+                    severity: "success",
+                    summary: "Добро пожаловать",
+                    detail: "Вход выполнен успешно!"
+                });
                 openState(false);
             }).catch((response) => {
-                console.log(response.response.data.errors[0]);
-                response.response.data.errors.forEach(function (error) {
-                    if (error.propertyName === 'EmailPhone') setUserLoginError(error.reason);
-                    if (error.propertyName === 'Password') setUserPasswordError(error.reason);
-                });
+                if (response.status === 400) {
+                    console.log(response.response.data.errors[0]);
+                    response.response.data.errors.forEach(function (error) {
+                        document.getElementById(`${error.propertyNames[0].toLowerCase()}-help`).innerText = error.reason;
+                    });
+                } else {
+                    console.log(response.response.data);
+                }
+                toast.current.show({ severity: "error", summary: "Ошибка авторизации" });
             });
         setIsLoading(false);
     }
