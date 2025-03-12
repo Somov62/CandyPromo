3432a4e00928a0b619cd829ce9d0e430069c102b
somov <somov@pulsarm.ru>
2025-03-12 22:45:39 +0300

    #13 Реализовал метод регистрации промокода за пользователем

---
+++ CandyPromo.Server/Controllers/PromocodeController.cs
@@ -0,0 +1,20 @@
+﻿using CandyPromo.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyPromo.Server.Controllers;
+
+/// <summary>
+/// Контроллер промокодов.
+/// </summary>
+public class PromocodeController(PromocodeService service) : BaseController
+{
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] string promocode, CancellationToken cancel)
+    {
+        var userId = GetUserId();
+
+        await service.RegisterByUser(userId, promocode, cancel);
+
+        return Ok();
+    }
+}
