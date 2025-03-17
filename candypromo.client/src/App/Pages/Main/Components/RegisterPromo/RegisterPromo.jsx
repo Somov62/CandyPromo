95640eacdc9b93c1d09649b912606e54919433d8
Дмитрий <dimanrusdevnet@outlook.com>
2025-03-17 13:09:40 +0300

    Add register Promo

--- candypromo.client/src/App/Pages/Main/Components/RegisterPromo/RegisterPromo.jsx
+++ candypromo.client/src/App/Pages/Main/Components/RegisterPromo/RegisterPromo.jsx
@@ -1,7 +1,14 @@
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+
 function RegisterPromo() {
     return (
-        <div className="products">
-            <h1>Регистрируй и выигрывай</h1>
+        <div className="products w-1/2 m-auto mt-10">
+            <p className="text-center font-bold text-4xl">
+                Зарегистрируйся и получи возможность выиграть крутые призы!
+            </p>
+            <InputText placeholder="Промокод" />
+            <Button className="w-1/1 mt-2" label="Зарегистрировать"/>
         </div >
     );
 }
