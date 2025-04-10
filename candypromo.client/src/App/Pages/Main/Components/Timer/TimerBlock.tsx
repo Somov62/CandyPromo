6063853cd2de1491e31ae5f5fda2ce4586ac7567
Дмитрий <dimanrusdevnet@outlook.com>
2025-04-10 22:06:44 +0300

    #58 Таймер вынесен в отдельный компонент

---
+++ candypromo.client/src/App/Pages/Main/Components/Timer/TimerBlock.tsx
@@ -0,0 +1,38 @@
+﻿import axios, { AxiosResponse } from "axios";
+import Timer from "../../../../Components/Timer/Timer";
+import "./TimerBlock.css";
+
+// #region Classes
+
+interface DatePrizeResult {
+    result: Date;
+    errors: any;
+    timeGenerated: Date;
+}
+
+// #endregion
+
+// #region Methods
+
+async function GetDatePrize(): Promise<number> {
+    try {
+        var result: AxiosResponse<DatePrizeResult> = await axios.get<DatePrizeResult>("/api/Promo/date");
+        return new Date(result.data.result).getTime();
+    }
+    catch {
+        return Date.now();
+    }
+}
+
+const date = await GetDatePrize();
+
+// #endregion
+
+export default function TimerBlock() {
+    return (
+        <div className="TimerWrapper flex-column mt-5 flex">
+            <h1>Отсчет до розыгрыша</h1>
+            <Timer endTime={date} />
+        </div>
+    );
+}
