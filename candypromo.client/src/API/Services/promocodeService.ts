09d6baf45dab8dd9470f26f9a8a6859d0f5fa694
Somov62 <somov@pulsarm.ru>
2025-05-12 21:24:28 +0300

    Начал реализацию страницы профиля с таблицы промокодов пользователя

--- candypromo.client/src/API/Services/promocodeService.ts
+++ candypromo.client/src/API/Services/promocodeService.ts
@@ -10,6 +10,10 @@ const promocodeService = {
     register (promocode: string) {
         return instance.post('api/promocode/register', promocode)
     },
+
+    getMyPromocodes() {
+        return instance.get('api/promocode')
+    }
 }
 
 export default promocodeService
--- candypromo.client/src/App/Pages/Profile/Profile.jsx
+++ candypromo.client/src/App/Pages/Profile/Profile.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Timer from "@/App/Pages/Main/Components/Timer/Timer.js";
+import Faq from "@/App/Pages/Main/Components/FAQ/FAQ.jsx";
+import PromocodeTable from "@/App/Pages/Profile/Components/PromocodeTable.jsx";
+import Footer from "@/App/Pages/Main/Components/Footer/Footer.jsx";
 
 const Profile = () => {
     return (
         <div>
             Профиль
+
+            <Timer/>
+            <PromocodeTable/>
+            <Faq/>
+            <Footer/>
         </div>
     );
 };
