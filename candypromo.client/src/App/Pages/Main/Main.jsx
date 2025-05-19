eb9ce0e5409c0582cb0c9187f21cb6c3b4887937
Дмитрий <dimanrusdevnet@outlook.com>
2025-05-19 21:00:33 +0300

    Исправлен отступ таймер ана главной странице и удален ненужный интерфейс

--- candypromo.client/src/App/Pages/Main/Main.jsx
+++ candypromo.client/src/App/Pages/Main/Main.jsx
@@ -1,7 +1,7 @@
 ﻿import "./Main.css"
 import ConditionsSection from "@/App/Pages/Main/Components/ConditionsSection/ConditionsSection.jsx";
 import Products from './Components/Products/Products';
-import Timer from "@/App/Pages/Main/Components/Timer/TimerBlock.tsx";
+import Timer from "./Components/Timer/TimerBlock.tsx";
 import Prizes from './Components/Prizes/Prizes';
 import RegisterPromo from "./Components/RegisterPromo/RegisterPromo";
 import Footer from "@/App/Components/Footer/Footer.jsx";
